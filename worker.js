bee6758b251bce607095d193ccdfac08a48f75d2
davidjoy <davjoy@gmail.com>
2019-12-16 22:12:45 -0500

    Adding eslint.

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
 const subscriptions = {};
-const nextToken = 0;
+let nextToken = 0;
 
 // SUBSCRIPTIONS
 
@@ -20,10 +20,11 @@ export function unsubscribe(type, token) {
 }
 
 
-
 function notifySubscribers(type, payload) {
     if (subscriptions[type] !== undefined) {
-        for (callback of Object.values(subscriptions[type])) {
+        const callbacks = Object.values(subscriptions[type]);
+        for (let i = 0; i < callbacks.length; i++) {
+            const callback = callbacks[i];
             callback(type, payload);
         }
     }
@@ -32,14 +33,14 @@ function notifySubscribers(type, payload) {
 export function dispatch(type, payload) {
     setTimeout(() => {
         global.postMessage({ type, payload });
-    }, 0)
+    }, 0);
 }
 
 // RECEIVING MESSAGES
 
 global.onmessage = function (event) {
-    const type = event.data.type;
-    const payload = event.data.payload;
+    const { type } = event.data;
+    const { payload } = event.data;
 
     notifySubscribers(type, payload);
-}
+};
